35166a8422d8bae7fc86e2df637f66ecfb4436d3
Chiara Orvati <chiara.orvati@epfl.ch>
2017-11-23 11:08:25 +0100

    chord diagram is working and interactive

--- chord.js
+++ chord.js
@@ -5,23 +5,29 @@ let margin = {
   right: 10
 }
 
-let width = 1200 - margin.left - margin.right;
-let height = 900 - margin.top - margin.bottom;
+let width = 1100 - margin.left - margin.right;
+let height = 1100 - margin.top - margin.bottom;
 let innerRadius = Math.min(width, height) * .25;
 let outerRadius = innerRadius * 1.1;
 
 let svg = d3.select('body').append('svg').attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom).attr("transform", "translate(" + margin.left + "," + margin.top + ")").append("g").attr("class", "chordDiagram").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-d3.csv('./data/adj_gname_weapon.csv', createChordDiagram);
+//************************************************************
+// CHANGE THE CSV FILE AND THE CHART IS COMPUTED AUTOMATICALLY
+//************************************************************
+d3.csv('./data/adjacency/adj_gname_targtype.csv', createChordDiagram);
 
 function createChordDiagram(data) {
 
   let firstColumn = 'gname';
 
+  let categories = Object.keys(data[0]);
+  categories = categories.slice(1,categories.length); // category names, i.e regions, etc
+
   let fc = data.map(function(d) {
       return d[firstColumn];
     }),
-    fo = fc.slice(0),
+    gnames = fc.slice(0), // terrorist groups
     matrix_size = (Object.keys(data[0]).length - 1) + fc.length,
     matrix = [];
 
@@ -56,56 +62,65 @@ function createChordDiagram(data) {
 
   let ribbon = d3.ribbon().radius(innerRadius);
 
-  //let fill = d3.schemeCategory20b;
   //let fill = d3.scaleLinear().domain([30, 38]).range(['#FFC300', '#581845']);
 
-  let fill = d3.schemeCategory10
+  let fill = d3.schemeCategory20;
+  // if(categories.length >10) {
+  //   fill = d3.schemeCategory20;
+  // }
 
+  console.log(fill)
 
-  let g = svg.append("g").attr('id', 'circle').datum(chord(matrix));
+  let g = svg.append("g").attr('class', 'circle').datum(chord(matrix));
 
-  let group = g.append("g").attr("class", "groups").selectAll("g").data(function(chords) {
+  let group = g.selectAll(".groups").data(function(chords) {
     return chords.groups;
-  }).enter().append("g");
+  }).enter().append('g').attr("class", "groups")
+  .on('mouseover', fade(0.05))
+  .on('mouseout', fade(1));
 
   group.append("path").style("fill", function(d, i) {
-    return (d.index) >= fo.length
-      ? fill[d.index-fo.length]
+    return (d.index) >= gnames.length
+      ? fill[d.index - gnames.length]
       : "#ccc";
   }).attr("d", arc);
 
-  g.append("g").attr("class", "ribbons").selectAll("path").data(function(chords) {
+  g.selectAll('.ribbons').data(function(chords) {
     return chords;
-  }).enter().append("path").attr("d", ribbon).style("fill", function(d) {
-    return fill[d.target.index-fo.length];
+  }).enter().append("path").attr("class", "ribbons").attr("d", ribbon).style("fill", function(d) {
+    return fill[d.target.index - gnames.length];
   });
 
+  svg.selectAll(".text").data(chordgroups).enter().append("text").attr("class", "text").attr("text-anchor", function(d) {
+    return d.angle > Math.PI
+      ? "end"
+      : null;
+  }).attr("transform", function(d) {
 
-  svg.selectAll(".text")
-            .data(chordgroups)
-            .enter()
-            .append("text")
-            .attr("class", "text")
-            .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-            .attr("transform", function(d){
+    //rotate each label around the circle
+    return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")" + "translate(" + (outerRadius + 10) + ")" + (d.angle > Math.PI
+      ? "rotate(180)"
+      : "");
 
-                //rotate each label around the circle
-                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")" +
-                       "translate(" + (outerRadius + 10) + ")" +
-                       (d.angle > Math.PI ? "rotate(180)" : "");
+  }).text(function(d, i) {
+    //set the text content
+    if (fc[i] == 'Vehicle (not to include vehicle-borne explosives, i.e., car or truck bombs)') {
+      return 'Vehicle';
+    } else {
+      return fc[i];
+    }
+  })
+  .style({
+    'font-family':'Verdana',
+    'font-size': '10px'
+  });
 
-            })
-            .text(function(d,i){
-                //set the text content
-                if (fc[i] == 'Vehicle (not to include vehicle-borne explosives, i.e., car or truck bombs)') {
-                  return 'Vehicle';
-                }else {
-                return fc[i];
-              }
-            })
-            .style({
-                "font-family":"sans-serif",
-                "font-size"  :"12px"
-            })
+  function fade(opacity) {
+    return function(g, i) {
+      svg.selectAll("path.ribbons").filter(function(d) {
+        return d.source.index != i && d.target.index != i;
+      }).transition().style("opacity", opacity);
+    };
+  }
 
 }
